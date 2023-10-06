f176c41fcda8bc97051c366a96ae8f2cf2517ac8
hanhbui1205 <145301247+hanhbui1205@users.noreply.github.com>
2023-10-05 23:26:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+//a button at the right end corner of the page, which brings back to top if clicked
+$("#back-to-top-button").click(function(){
+    $( "html" ).scrollTop(0);
+});
+})
